baec63f4c82e49e3021ebffc9b2ba00fef0dd889
Lunar <lunarlifeburst@gmail.com>
2026-05-03 02:03:33 +1000

    monaco editor

--- index.ts
+++ index.ts
@@ -2,11 +2,20 @@ import runGo from './go'
 import testLesson from './lesson1.md' with {type: "text"}
 import parseLesson from './lessonParser'
 
+import * as monaco from 'monaco-editor'
+
 const lesson = parseLesson(testLesson)
-document.body.innerHTML = JSON.stringify(lesson)
+
 window.lesson = lesson
 
 
+const editor = monaco.editor.create(document.getElementById("editor")!, {
+    value: lesson.template,
+    language: "go",
+    theme: "vs-dark",
+    minimap: { enabled: false },
+    automaticLayout: true,
+})
 
 // document.write(runGo(`
 //     package main
